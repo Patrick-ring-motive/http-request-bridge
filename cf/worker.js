442ad33dee82cee593d0211506a230a1b0be7411
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-09-07 14:36:52 -0500

    style: js-beautify formatting (cf/worker.js)

--- cf/worker.js
+++ cf/worker.js
@@ -1,51 +1,50 @@
-  
-const fetchResponse = async (...args) => {
-  try {
-    return await fetch(...args);
-  } catch (e) {
-    return new Response(String(e), {
-      status: 500,
-      statusText: String(e)
-    });
-  }
-};
-
-const fetchTextResponse = async (...args) => {
-  try {
-    const res = await fetchResponse(...args);
-    const body = await res.text();
-    return new Response(body,res);
-  } catch (e) {
-    return new Response(String(e), {
-      status: 500,
-      statusText: String(e)
-    });
-  }
-};
-
-const transact = async(payload,env)=>{
-  const req = new Request(`${env?.WIX_HOST}/request`,{
-    method:"POST",
-    headers:{
-      "transaction-id":`transaction-${crypto.randomUUID()}`,
-      "transaction-status":"started",
-      "transaction-creates":Date.now()
-    },
-    body:payload
-  });
-  return fetchTextResponse(req);
-};
-
-export default {
-  async fetch(request, env, ctx) {
+  const fetchResponse = async (...args) => {
     try {
-      const payload = request.body ? (await request.text()) : request.url;
-      return await transact(payload,env);
+      return await fetch(...args);
     } catch (e) {
       return new Response(String(e), {
         status: 500,
         statusText: String(e)
       });
     }
-  },
-};
+  };
+
+  const fetchTextResponse = async (...args) => {
+    try {
+      const res = await fetchResponse(...args);
+      const body = await res.text();
+      return new Response(body, res);
+    } catch (e) {
+      return new Response(String(e), {
+        status: 500,
+        statusText: String(e)
+      });
+    }
+  };
+
+  const transact = async (payload, env) => {
+    const req = new Request(`${env?.WIX_HOST}/request`, {
+      method: "POST",
+      headers: {
+        "transaction-id": `transaction-${crypto.randomUUID()}`,
+        "transaction-status": "started",
+        "transaction-creates": Date.now()
+      },
+      body: payload
+    });
+    return fetchTextResponse(req);
+  };
+
+  export default {
+    async fetch(request, env, ctx) {
+      try {
+        const payload = request.body ? (await request.text()) : request.url;
+        return await transact(payload, env);
+      } catch (e) {
+        return new Response(String(e), {
+          status: 500,
+          statusText: String(e)
+        });
+      }
+    },
+  };
