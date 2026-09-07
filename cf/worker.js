d9639564965df6b82ae51bc2ca866ea523e06e0d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-09-07 13:21:22 -0500

    Update worker.js

--- cf/worker.js
+++ cf/worker.js
@@ -1,1 +1,32 @@
   
+const fetchResponse = async (...args) => {
+  try {
+    return await fetch(...args);
+  } catch (e) {
+    return new Response(String(e), {
+      status: 500,
+      statusText: String(e)
+    });
+  }
+};
+
+const fetchTextResponse = async (...args) => {
+  try {
+    const res = await fetchResponse(...args);
+    const body = await res.text();
+    return new Response(body,res);
+  } catch (e) {
+    return new Response(String(e), {
+      status: 500,
+      statusText: String(e)
+    });
+  }
+};
+
+
+
+export default {
+  async fetch(request, env, ctx) {
+    return fetchResponseText(request);
+  },
+};
