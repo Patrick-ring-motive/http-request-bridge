0afdc938e2803afb11f1f44f8ada76696b4b8001
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-09-09 16:45:01 -0500

    style: js-beautify formatting (wix/bridge.js)

--- wix/bridge.js
+++ wix/bridge.js
@@ -1,32 +1,41 @@
-
-import { ok, serverError } from 'wix-http-functions';
+import {
+  ok,
+  serverError
+} from 'wix-http-functions';
 import wixData from 'wix-data';
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-class CMSTable{
-  constructor(tableName){
+class CMSTable {
+  constructor(tableName) {
     this.tableName = tableName;
   }
-  async where(prop,value){
+  async where(prop, value) {
     return await wixData
       .query(this.tableName)
-      .eq(prop,value)
+      .eq(prop, value)
       .find();
   }
-  async insert(value){
-      return await wixData.insert(this.tableName,value,{ suppressAuth: true })
+  async insert(value) {
+    return await wixData.insert(this.tableName, value, {
+      suppressAuth: true
+    })
   }
-  async update(id,value){
-      return await wixData.update(this.tableName,{...id,...value},{ suppressAuth: true })
+  async update(id, value) {
+    return await wixData.update(this.tableName, {
+      ...id,
+      ...value
+    }, {
+      suppressAuth: true
+    })
   }
 }
 
 const requests = new CMSTable('requests');
 const responses = new CMSTable('responses');
 
-async function handleRequest(fn,args=[]){
-  try{
+async function handleRequest(fn, args = []) {
+  try {
     return await fn(...args);
   } catch (err) {
     return serverError({
@@ -37,42 +46,42 @@ async function handleRequest(fn,args=[]){
   }
 }
 
-const jsonResponse = x =>{
+const jsonResponse = x => {
   return ok({
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin":"*"
-      },
-      body: x
-    });
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: x
+  });
 };
 
 export async function get_items(request) {
-  return handleRequest(async()=>{
+  return handleRequest(async () => {
     const results = await wixData
       .query("env")
       .limit(100)
       .find();
 
     return jsonResponse({
-        items: results.items,
-        totalCount: results.totalCount
-      });
+      items: results.items,
+      totalCount: results.totalCount
+    });
   });
 }
 
 export async function get_requests(request) {
-  return handleRequest(async()=>{
-    const results = await responses.where('transaction_status','started');
+  return handleRequest(async () => {
+    const results = await responses.where('transaction_status', 'started');
     return jsonResponse({
-        items: results.items,
-        totalCount: results.totalCount
-      });
+      items: results.items,
+      totalCount: results.totalCount
+    });
   });
 }
 
 export async function post_request(request) {
-  return handleRequest(async()={
+  return handleRequest(async () = {
     const transactionId = request.headers['transaction-id'] || `transaction-${Math.random()}`;
     await requests.insert({
       transaction_id: transactionId,
@@ -80,54 +89,65 @@ export async function post_request(request) {
       transaction_created: +request.headers['transaction-created'],
       payload: await (await request.body).text(),
     });
-    let result = await responses.where('transaction_id',transactionId);
-    while(!result?.totalCount){
+    let result = await responses.where('transaction_id', transactionId);
+    while (!result?.totalCount) {
       await sleep(1000);
-      result = await responses.where('transaction_id',transactionId);
+      result = await responses.where('transaction_id', transactionId);
     }
     return jsonResponse(result);
   });
 }
 
-import { elevate } from 'wix-auth';
-import { collections } from 'wix-data.v2';
+import {
+  elevate
+} from 'wix-auth';
+import {
+  collections
+} from 'wix-data.v2';
 
 const listDataCollections = elevate(collections.listDataCollections);
 
 export async function get_schemas(request) {
-  return handleRequest(async()={
+  return handleRequest(async () = {
     const result = await listDataCollections();
-    return jsonResponse(result.collections.filter(x=/request|responses/.test(x.displayName)).map(x=x.fields.filter(x=!x.systemField).map(y=y.displayName)));
+    return jsonResponse(result.collections.filter(x = /request|responses/.test(x.displayName)).map(x = x.fields.filter(x = !x.systemField).map(y = y.displayName)));
   });
 }
 
-export async function post_response(request){
+export async function post_response(request) {
   const items = JSON.parse(await (await request.body).text());
-  for(const item of items){
-        const results = await requests.where('transaction_id',item.transaction_id);
-        for(const req of results?.items ?? []){
-          await requests.update(req,{transaction_status:'done'});
-        }
-        await responses.insert(item);
+  for (const item of items) {
+    const results = await requests.where('transaction_id', item.transaction_id);
+    for (const req of results?.items ?? []) {
+      await requests.update(req, {
+        transaction_status: 'done'
+      });
+    }
+    await responses.insert(item);
   }
   return ok();
 }
 
-export async function get_listen(){
-  while(true){
-    try{
-        const results = await requests.where('transaction_status','started');
-        for(const req of results?.items ?? []){
-          await requests.update(req,{transaction_status:'claimed'});
-          await responses.insert({transaction_id:req.transaction_id,response:'claimed'});
-        }
-        if(results?.items?.length){
-          return jsonResponse(results);
-        }else{
-          await sleep(1000);
-        }
-    }catch(e){
-        console.warn(e);
+export async function get_listen() {
+  while (true) {
+    try {
+      const results = await requests.where('transaction_status', 'started');
+      for (const req of results?.items ?? []) {
+        await requests.update(req, {
+          transaction_status: 'claimed'
+        });
+        await responses.insert({
+          transaction_id: req.transaction_id,
+          response: 'claimed'
+        });
+      }
+      if (results?.items?.length) {
+        return jsonResponse(results);
+      } else {
+        await sleep(1000);
+      }
+    } catch (e) {
+      console.warn(e);
     }
   }
 }
